6f6c51aa24e930bb84df0318c69f0837d58887b9
Daniel Wiśniewski <kontakt.danielwisniewski@gmail.com>
2020-12-11 01:53:56 +0100

    update landing page it will show logo and welcome tekst

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,18 +1,31 @@
 import React from "react";
 import styled from "styled-components";
-import logo from '../../images/wolf_logo.jpg'
+import logo from "../../images/wolf_logo.jpg";
+import text from "./text";
 
 const LandingPage = () => {
-  return <Landing>
-      <img src={logo}/>
-  </Landing>;
+  return (
+    <Landing>
+      <Image src={logo} />
+      <Title>{text.text}</Title>
+    </Landing>
+  );
 };
 
 const Landing = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
 
+const Image = styled.img`
+  width: 250px;
+`;
+
+const Title = styled.h4`
+width: 50%;
+text-align:center;
+`;
 
 export default LandingPage;
---
+++ src/components/LandingPage/text.js
@@ -0,0 +1,3 @@
+export default {
+    text: 'Witaj w świecie, w którym Ty decydujesz co jest ważne. Usiądż z nami i rozpocznij przygodę'
+}
