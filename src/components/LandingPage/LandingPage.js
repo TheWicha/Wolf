70bed1764f660c79a6ebb2a1f7717671cc694b58
Daniel Wiśniewski <kontakt.danielwisniewski@gmail.com>
2020-12-23 15:25:15 +0100

    Add landingPage it will display welcoming information on our platform

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,14 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import logo from "../../images/wolf_logo.jpg";
 import text from "./text";
-import colors from "../../colors/colors";
+import Button from "../common/Button";
 
 const LandingPage = () => {
   return (
     <Landing>
       <Image src={logo} />
       <Title>{text.text}</Title>
-      <Button>Zaczynamy</Button>
+      <Button desc={"Zaczynamy"} />
     </Landing>
   );
 };
@@ -28,22 +28,6 @@ const Image = styled.img`
 const Title = styled.h4`
   width: 50%;
   text-align: center;
-`;
-
-const Button = styled.button`
-  padding: 16px;
-  font-size: 15px;
-  background-color: ${colors.mainDark};
-  color: ${colors.mainGray};
-  border-radius: 8px;
-  border: none;
-  outline: none;
-  cursor: pointer;
-  font-weight: 600;
-  margin-top: 20px;
-  &:hover{
-      background-color: ${colors.mainBlue}
-  }
 `;
 
 export default LandingPage;
---
+++ src/components/common/Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+import colors from "../../colors/colors"
+
+
+const Button = ({desc}) => {
+return <StyledButton>{desc}</StyledButton>;
+}
+
+
+export default Button;
+
+const StyledButton = styled.button`
+  padding: 16px;
+  font-size: 15px;
+  background-color: ${colors.mainDark};
+  color: ${colors.mainGray};
+  border-radius: 6px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  font-weight: 600;
+  margin-top: 20px;
+  &:hover{
+      background-color: ${colors.mainBlue};
+  }`
